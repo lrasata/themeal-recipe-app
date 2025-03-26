298efe5b18632fdd35d22037af9998f9d8b7f2fe
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-03-26 16:53:59 +0100

    fix: installing aws amplify again

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,7 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'iam',
   },
 });
 
